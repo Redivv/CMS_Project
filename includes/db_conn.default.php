50a28dd83954779a3035ced7596f8c0f78269dd1
Redivv <j.rajca45@gmail.com>
2019-06-16 14:24:04 +0200

    Deploy Changes

--- includes/db_conn.default.php
+++ includes/db_conn.default.php
@@ -1,12 +1,10 @@
 <?php
 declare(strict_types = 1);
-include ('C:\xampp\htdocs\Projects\CMS_Project\admin\processing\functions.php');
-ob_start();      // aktywuje output buffering przez co output ze skryptu poza header jest blokowany
 
-$db['db_host'] = 'localhost';        // we create an array of database login data
-$db['db_username'] = 'wipaka_rajczogli';
-$db['db_password'] = 'kauczuk1';
-$db['db_database'] = 'cmd_udemy';
+$db['db_host'] = '';        // we create an array of database login data
+$db['db_username'] = '';
+$db['db_password'] = '';
+$db['db_database'] = '';
 
 foreach ($db as $key => $value) {      // then we convert each index into an constant
   define(strtoupper($key),$value);
